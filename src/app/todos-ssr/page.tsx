884927bacdd83dee6a840af652d01bb81793712b
기다림 <gidalimforsomeday@gmail.com>
2024-03-12 12:18:39 +0900

    [ADD] create types

--- src/app/todos-ssr/page.tsx
+++ src/app/todos-ssr/page.tsx
@@ -2,10 +2,16 @@
 // CUD 안 함
 // Link태그를 통한 report페이지 이동 '할 일 정보 통계 보러가기' 페이지
 
+import Link from "next/link";
 import React from "react";
 
 const todosSSR = () => {
-  return <div>todosSSR</div>;
+  return (
+    <div>
+      <Link href={"/report"}>할 일 통계 보러가기</Link>
+      todosSSR
+    </div>
+  );
 };
 
 export default todosSSR;
---
+++ src/types/type.ts
@@ -0,0 +1,11 @@
+export type Todos = {
+  title: string;
+  contents: string;
+  isDone: boolean;
+};
+
+export type CompanyInfo = {
+  name: string;
+  description: string;
+  image: string;
+};
