51d72001143d2ef14a5b85ac309173de75037536
기다림 <gidalimforsomeday@gmail.com>
2024-03-13 19:50:32 +0900

    [MOD] add environment setting

--- src/hooks/useTodoQuery.tsx
+++ src/hooks/useTodoQuery.tsx
@@ -2,10 +2,11 @@
 
 import { Todo } from "@/types/type";
 import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "./keys.constant";
 
 export const useTodoQuery = () => {
   return useQuery<Todo[]>({
-    queryKey: ["todos"],
+    queryKey: [QUERY_KEYS.TODOLIST],
     queryFn: async () => {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_HOST_URL}api/todos`
